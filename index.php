44402986be3e4b73bf1955780086995278cbd236
mirko93 <skopljak.mirko93@gmail.com>
2020-02-18 23:01:03 +0100

    "18-02-2020 - first push project"

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Google</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <div class="wrapper indexPage">
+        <div class="mainSection">
+            <div class="logoContainer">
+                <img src="https://www.google.rs/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+            </div>
+
+            <div class="searchContainer">
+                <form action="search.php" method="get">
+                    <input class="searchBox" type="text" name="term">
+                    <input class="searchButton" type="submit" value="Search">
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,75 @@
+<?php
+
+if (isset($_GET["term"])) {
+    $term = $_GET["term"];
+} else {
+    exit("You must enter a search term");
+}
+
+if (isset($_GET["type"])) {
+    $type = $_GET["type"];
+} else {
+    $type = "sites";
+}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Google</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <div class="wrapper">
+        <div class="header">
+            <div class="headerContent">
+
+                <div class="logoContainer">
+                    <a href="index.php">
+                        <img src="assets/image/doodleLogo.png">
+                    </a>
+                </div>
+
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+                        <div class="searchBarContainer">
+                            <input class="searchBox" type="text" name="term">
+                            <button class="searchButton">
+                                <img src="assets/image/icons/search.png">
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="tabsContainer">
+                <ul class="tabList">
+
+                    <li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
+                        <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+                            Sites
+                        </a>
+                    </li>
+
+                    <li class="<?php echo $type == 'images' ? 'active' : '' ?>">
+                        <a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+                            Images
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
