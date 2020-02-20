42f6eb4a113f4292d6d2b05387396416dab0cbe6
mirko93 <skopljak.mirko93@gmail.com>
2020-02-20 20:21:33 +0100

    "20-02-2020 - function search -done"

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -41,7 +41,7 @@ class SiteResultsProvider {
 
             $resultsHtml .= "<div class='resultContainer'>
                                 <h3 class='title'>
-                                    <a href='url' class='result'>$title</a>
+                                    <a href='$url' class='result'>$title</a>
                                 </h3>
                                 <span class='url'>$url</span>
                                 <span class='$description'>$description</span>
